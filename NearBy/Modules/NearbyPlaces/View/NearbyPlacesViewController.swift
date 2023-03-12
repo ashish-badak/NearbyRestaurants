696fd2680aaafd74c386c2b874e4dae8f57a5f80
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-03-12 16:14:24 +0530

    added nearby places view controller

---
+++ NearBy/Modules/NearbyPlaces/View/NearbyPlacesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NearbyPlacesViewController.swift
+//  NearBy
+//
+//  Created by Ashish Badak on 12/03/23.
+//
+
+import UIKit
+
+class NearbyPlacesViewController: UIViewController {
+    
+    private var presenter: NearbyPlacesPresenterProtocol
+    
+    init(presenter: NearbyPlacesPresenterProtocol) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        // - TODO: setup view components
+    }
+}
