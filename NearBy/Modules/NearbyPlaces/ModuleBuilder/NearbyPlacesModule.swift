ca19df04cfbe911115d333a59d5cf5f5f038fcaf
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-03-12 16:10:12 +0530

    defined NearbyPlacesModule

---
+++ NearBy/Modules/NearbyPlaces/ModuleBuilder/NearbyPlacesModule.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyPlacesModule.swift
+//  NearBy
+//
+//  Created by Ashish Badak on 12/03/23.
+//
+
+import UIKit
+
+final class NearbyPlacesModule {
+    // - TODO: Accept dependancies inside build method as parameters
+    func build() -> UIViewController {
+        // - TODO: Build nearby places controller with dependancies here and return it
+        let controller = UIViewController()
+        return controller
+    }
+}
