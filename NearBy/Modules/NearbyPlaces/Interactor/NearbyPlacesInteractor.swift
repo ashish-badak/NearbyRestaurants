5870724a80d33e7dffedc637cff292c47e6709b4
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-03-12 19:56:49 +0530

    defined NearbyPlacesInteractor

---
+++ NearBy/Modules/NearbyPlaces/Interactor/NearbyPlacesInteractor.swift
@@ -0,0 +1,12 @@
+//
+//  NearbyPlacesInteractor.swift
+//  NearBy
+//
+//  Created by Ashish Badak on 12/03/23.
+//
+
+import Foundation
+
+final class NearbyPlacesInteractor: NearbyPlacesInteractorProtocol {
+    // - TODO: Inject service layer as a dependancy once created
+}
