ba8825a0819d7f0a0df8132b821528d029268d61
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-03-12 14:10:37 +0530

    folders rearranged

---
+++ NearBy/ApplicationStartup/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  NearBy
+//
+//  Created by Ashish Badak on 12/03/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
