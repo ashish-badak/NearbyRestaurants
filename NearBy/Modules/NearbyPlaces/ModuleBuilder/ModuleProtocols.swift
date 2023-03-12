4e43c928043fa570e7cd51a6e1f5833a61037253
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-03-12 16:06:47 +0530

    defined nearby places module protocols

---
+++ NearBy/Modules/NearbyPlaces/ModuleBuilder/ModuleProtocols.swift
@@ -0,0 +1,24 @@
+//
+//  ModuleProtocols.swift
+//  NearBy
+//
+//  Created by Ashish Badak on 12/03/23.
+//
+
+import Foundation
+
+protocol NearbyPlacesViewProtocol: AnyObject {
+    // - TODO: Define view requirements
+}
+
+protocol NearbyPlacesInteractorProtocol: AnyObject {
+    // - TODO: Define interactor requirements
+}
+
+protocol NearbyPlacesPresenterProtocol: AnyObject {
+    // - TODO: Define presenter requirements
+}
+
+protocol NearbyPlacesRouterProtocol: AnyObject {
+    // - TODO: Define router requirements
+}
