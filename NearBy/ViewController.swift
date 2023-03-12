0c86ab6c99c0db250616ba6b10fd45846ee1d131
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2023-03-12 14:02:41 +0530

    created xcode project

---
+++ NearBy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearBy
+//
+//  Created by Ashish Badak on 12/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
